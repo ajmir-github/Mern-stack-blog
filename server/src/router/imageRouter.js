e6241a60d55377f94ef8b878a3a98d6c58a4b4cd
Ajmir Raziqi <ajmir.ng3@gmail.com>
2022-11-16 09:25:03 -0800

    image route were refactored and tested

--- server/src/router/imageRouter.js
+++ server/src/router/imageRouter.js
@@ -1,28 +1,44 @@
 // imports
 const express = require("express");
 const router = express.Router();
+const { v4: uuid } = require("uuid");
+const path = require("path");
 const {
   getImage,
   uploadImage,
-  deleteImage
+  deleteImage,
 } = require("../controller/imageController");
 
+// vars
+const ImgFolder = "image";
+const PublicDir = path.join(__dirname, "../", "public");
+const ImgSizes = {
+  xxxs: 32,
+  xxs: 64,
+  xs: 126,
+  sm: 256,
+  md: 512,
+  lg: 600,
+  xl: 720,
+  xxl: 920,
+  xxxl: 1280,
+};
 
-router.get("/:fileName", // get image when needed convert them
-  getImage
+// Routes
+router.get(
+  "/:fileName", // get image when needed convert them
+  getImage(PublicDir, ImgFolder, ImgSizes)
 );
 
-router.post("/", // upload an image with a unique name
-  uploadImage
+router.post(
+  "/", // upload an image with a unique name
+  uploadImage(PublicDir, ImgFolder, "file", uuid)
 );
 
-
-router.delete("/:fileName",  // delete an image with all its coverted forms
-  deleteImage
+router.delete(
+  "/:fileName", // delete an image with all its coverted forms
+  deleteImage(PublicDir, ImgFolder)
 );
-
-
-
 
 // exports
 module.exports = router;
